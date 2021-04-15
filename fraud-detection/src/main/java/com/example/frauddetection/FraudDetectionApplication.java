9e1dfd6ce4f229142ee1bd31c306fb0073a0c83e
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-04-15 09:43:36 +0200

    Updated fraud with missing controller

--- fraud-detection/src/main/java/com/example/frauddetection/FraudDetectionApplication.java
+++ fraud-detection/src/main/java/com/example/frauddetection/FraudDetectionApplication.java
@@ -1,5 +1,7 @@
 package com.example.frauddetection;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.slf4j.Logger;
@@ -7,6 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FraudDetectionApplication {
@@ -24,5 +28,17 @@ public class FraudDetectionApplication {
 	// frauds-in-0
 	Consumer<String> frauds() {
 		return s -> log.info("Got a message with body [" + s + "]");
+	}
+}
+
+@RestController
+class FraudDetectionController {
+
+	private static final Logger log = LoggerFactory.getLogger(FraudDetectionController.class);
+
+	@GetMapping("/frauds")
+	List<String> frauds() {
+		log.info("\n\nGot fraud request\n\n");
+		return Arrays.asList("josh", "marcin");
 	}
 }
